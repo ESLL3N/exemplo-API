601222e434aba6408898d422ff8ee8a73ed47bad
ESLL3N <esllenescolar@gmail.com>
2021-11-11 17:55:35 +0000

    banco de dados

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,6 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes (titulo) values 
+    ('Entre Facas e Segredos'),
+    ('A Sociedade do Anel'),
+    ('O Hobbit');
